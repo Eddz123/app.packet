2dba484a92b84a5976a6e4ac48ce4696f6636e9a
Eddz123 <41518972+Eddz123@users.noreply.github.com>
2020-03-15 18:10:39 +0800

    New Update

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ var sess = {
   cookie: {}
 };
 
+
 if (app.get('env') === 'production') {
-  app.set('trust proxy', 1) // trust first proxy
+  app.set('trust proxy', 1); // trust first proxy
   sess.cookie.secure = true // serve secure cookies
 }
 
