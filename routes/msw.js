adc2a205d5092d17aec8f0f2c829ee51296769ba
Eddz123 <41518972+Eddz123@users.noreply.github.com>
2020-05-06 00:51:02 +0800

    New Update

--- routes/msw.js
+++ routes/msw.js
@@ -7,22 +7,22 @@ const main = require('./main');
 //------------------------------- MSW START ----------------------------------//
 
 
-//Get the List of Applicant In Database
+//Get the List of Applicant form Database
 router.get('/',(req,res,next)=>{
-  if(req.session.email){
-    var g = `SELECT * FROM msw_information `;
-    con.query(g,(err,rs)=>{
-      if(err){
-        console.log(err);
-      }
+    if(req.session.email){
+        var g = `SELECT * FROM msw_information `;
+        con.query(g,(err,rs)=>{
+            if(err){
+                console.log(err);
+            }
 
-      res.render('mswtable',{ title : 'Applicant List', applicationpacket : rs});
-      res.end();
-    });
-  }else{
-    res.redirect('/users/login');
-    res.end();
-  }
+            res.render('mswtable',{ title : 'Applicant List', applicationpacket : rs});
+            res.end();
+        });
+    }else{
+        res.redirect('/users/login');
+        res.end();
+    }
 });
 
 
